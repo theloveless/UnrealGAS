bf01177d9f01e58f4eff73e63c3a36f6de901fc7
theloveless <mattiaspersson@outlook.com>
2024-02-07 14:30:18 +0000

    Added AuraCharacter and AuraEnemy classes. Made AuraCharacterBase class abstract

--- Source/Aura/Public/Characters/AuraCharacterBase.h
+++ Source/Aura/Public/Characters/AuraCharacterBase.h
@@ -1,4 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -6,24 +5,19 @@
 #include "GameFramework/Character.h"
 #include "AuraCharacterBase.generated.h"
 
-UCLASS()
+UCLASS(Abstract) //Abstract classes cannot be dragged into a level.
 class AURA_API AAuraCharacterBase : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
+
 	AAuraCharacterBase();
 
 protected:
-	// Called when the game starts or when spawned
+
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
