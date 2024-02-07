a02538c1ef1c2be571e28d1a1c7fd82e38332a12
theloveless <mattiaspersson@outlook.com>
2024-02-07 13:34:55 +0000

    Fixes of character class, made .cpp private

--- Source/Aura/Private/Characters/AuraCharacterBase.cpp
+++ Source/Aura/Private/Characters/AuraCharacterBase.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Characters/Public/AuraCharacterBase.h"
+#include "Characters/AuraCharacterBase.h"
 
 // Sets default values
 AAuraCharacterBase::AAuraCharacterBase()
