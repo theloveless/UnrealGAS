e705649109080d169814891cea74d42e38729150
theloveless <mattiaspersson@outlook.com>
2024-02-09 17:42:21 +0000

    Custom depth for mesh and weapon mesh

--- Source/Aura/Private/Characters/AuraEnemy.cpp
+++ Source/Aura/Private/Characters/AuraEnemy.cpp
@@ -3,12 +3,34 @@
 
 #include "Characters/AuraEnemy.h"
 
+#include "Aura.h"
+
+AAuraEnemy::AAuraEnemy()
+{
+	if(GetMesh() != nullptr)
+	{
+		GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	}
+
+	if(Weapon != nullptr)
+	{
+		Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	}
+
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility,ECR_Block);
+}
+
 void AAuraEnemy::HighlightActor()
 {
 	bHighlighted = true;
+
+	GetMesh()->SetRenderCustomDepth(true);
+	Weapon->SetRenderCustomDepth(true);
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
 	bHighlighted = false;
+	GetMesh()->SetRenderCustomDepth(false);
+	Weapon->SetRenderCustomDepth(false);
 }
